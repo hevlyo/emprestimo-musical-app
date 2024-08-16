d2259ee476f9726f105b60270095e49a3ce40704
hevlyo <riquelme.pereira1@gmail.com>
2024-08-15 22:42:51 -0300

    Mudando a configuração do firebase para usar .env

--- app/src/firebaseConnection.js
+++ app/src/firebaseConnection.js
@@ -1,22 +1,18 @@
-// Import the functions you need from the SDKs you need
+import { API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
 import { initializeApp } from "firebase/app";
 
 import {getDatabase, ref, set, push, onValue} from 'firebase/database';
 import { initializeAuth, getReactNativePersistence} from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9nrgTIfxbfnTbW7XhjS5q-0ghn4-N-dQ",
-  authDomain: "emprestimos-ufrn.firebaseapp.com",
-  databaseURL: "https://emprestimos-ufrn-default-rtdb.firebaseio.com",
-  projectId: "emprestimos-ufrn",
-  storageBucket: "emprestimos-ufrn.appspot.com",
-  messagingSenderId: "615663884758",
-  appId: "1:615663884758:web:787cf185d744ceeeae6c77"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
